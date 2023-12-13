770ef0ff50646297316adc08f50a35343456ccf7
toomyem <toomyem@gmail.com>
2023-12-13 21:26:07 +0100

    Day 12

--- day12/Main.java
+++ day12/Main.java
@@ -1,6 +1,8 @@
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -11,72 +13,71 @@ public class Main {
     void run() throws Exception {
         try (Scanner in = new Scanner(new File("input.txt"))) {
 
-            int sum1 = 0;
+            long sum1 = 0;
+            long sum2 = 0;
             while (in.hasNext()) {
                 String line = in.nextLine();
                 String[] parts = line.split(" ");
                 String query = parts[0];
                 String rules = parts[1];
-                sum1 += solve(query, rules);
+                sum1 += solve(query, rules, 0);
+                sum2 += solve(unfold(query, "?"), unfold(rules, ","), 0);
             }
             System.out.println("Part 1: " + sum1);
+            System.out.println("Part 2: " + sum2);
+            System.out.println("Memo size: " + memo.size());
         }
     }
 
-    int solve(String query, String rules) {
-        int q = countQuestionMarks(query);
-        int n = 1 << q;
-        int m = 0;
+    final Map<String, Long> memo = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            String r = replace(query, i);
-            String g = groups(r);
-            if (g.equals(rules)) m++;
-        }
-        System.out.println(query + " -> " + m);
-        return m;
-    }
+    long solve(String query, String rules, int n) {
+        String key = query + "/" + rules + "/" + n;
+        Long sum = memo.get(key);
+        if (sum != null) return sum;
 
-    String groups(String s) {
-        List<String> result = new ArrayList<>();
-        int g = 0;
-        int i = 0;
-        while (i <= s.length()) {
-            char ch = i < s.length() ? s.charAt(i) : '.';
-            if (ch == '#') {
-                g++;
-            } else {
-                if (g > 0) {
-                    result.add("" + g);
-                    g = 0;
+        List<Integer> groups = Arrays.stream(rules.split(",")).mapToInt(Integer::parseInt).boxed().toList();
+        sum = 0L;
+        int size = groups.get(n);
+        for (int i = 0; i <= query.length() - size; i++) {
+            if (spaces(query, 0, i) && fits(query.substring(i), size)) {
+                if (n == groups.size() - 1) {
+                    int remaining = query.length() - size - i;
+                    if (spaces(query, i + size, remaining)) {
+                        sum += 1;
+                    }
+                } else if (i + size + 1 < query.length()) {
+                    sum += solve(query.substring(i + size + 1), rules, n + 1);
                 }
             }
-            i++;
         }
-        return String.join(",", result);
+
+        memo.put(key, sum);
+        return sum;
     }
 
-    String replace(String s, int n) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            char r;
-            if (ch == '?') {
-                r = n % 2 == 0 ? '.' : '#';
-                n = n / 2;
-            } else {
-                r = ch;
-            }
-            result += r;
+    boolean spaces(String query, int s, int n) {
+        for (int i = 0; i < n; i++) {
+            if (query.charAt(s + i) == '#') return false;
+        }
+        return true;
+    }
+
+    boolean fits(String query, int size) {
+        int i = 0;
+        while (i < size) {
+            char ch = query.charAt(i);
+            if (ch == '.') return false;
+            i += 1;
+        }
+        return i >= query.length() || query.charAt(i) != '#';
+    }
+
+    String unfold(String s, String delimiter) {
+        String result = s;
+        for (int i = 1; i < 5; i++) {
+            result = result + delimiter + s;
         }
         return result;
-    }
-
-    int countQuestionMarks(String s) {
-        int n = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '?') n++;
-        }
-        return n;
     }
 }
